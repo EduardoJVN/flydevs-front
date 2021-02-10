e95709b2a9f59127838b7e9260013ac813676b91
theBalckWolfTech <ejvn30@gmail.com>
2021-02-10 19:16:06 -0300

    upload test

--- src/services/api/index.test.js
+++ src/services/api/index.test.js
@@ -8,24 +8,24 @@ it("getPokemons should return a list of 20 pokemons", async ()=>{
 })
 
 
-// it("searchPokemons should bring an array with the pokemons according to the parameter set by the user", async ()=>{
-//     const parameters = [
-//         "pika",
-//         "ditto",
-//         "bulbasaur",
-//         "charizard",
-//         "+",
-//         ""
-//     ]
-//     for(let index in parameters){
-//         const response = await searchPokemons(parameters[index]);
-//         expect(response).toBeDefined()
-//         expect(response).not.toBeNull()
-//         if(index == 4){
-//             expect(response.message.length).toEqual(0)
-//         }else{
-//             expect(response.message.length).toBeGreaterThanOrEqual(1)
-//         }
-//     }
+it("searchPokemons should bring an array with the pokemons according to the parameter set by the user", async ()=>{
+    const parameters = [
+        "pika",
+        "ditto",
+        "bulbasaur",
+        "charizard",
+        "+",
+        ""
+    ]
+    for(let index in parameters){
+        const response = await searchPokemons(parameters[index]);
+        expect(response).toBeDefined()
+        expect(response).not.toBeNull()
+        if(index == 4){
+            expect(response.message.length).toEqual(0)
+        }else{
+            expect(response.message.length).toBeGreaterThanOrEqual(1)
+        }
+    }
     
-// })
+})
